718c7a60d4bab89da447cb083240f6be373c256d
Matheus Santana <matheus11_santana@hotmail.com>
2021-01-29 16:41:36 -0300

    Adição do update para Requisições PUT

--- src/main/java/com/example/endpoint/StudentEndpoint.java
+++ src/main/java/com/example/endpoint/StudentEndpoint.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,12 @@ public class StudentEndpoint {
 		Student.studentList.remove(student);
 		return new ResponseEntity<>(student,HttpStatus.OK);
 	}
-
-
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student){
+		Student.studentList.remove(student);
+		Student.studentList.add(student);
+		return new ResponseEntity<>(student,HttpStatus.OK);
+	}
+	
+	
 }
